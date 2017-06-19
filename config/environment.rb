f07a8b363fde7512d91b7cbd1867f331a1d8c3db
satchmo2002 <satchwill.aaron@gmail.com>
2017-06-19 21:21:12 +0000

    messing up on purpose

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['SENDGRID_gkkkUSERNAME'],
+    :password => ENV['SENDGRID_sfdfPASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
