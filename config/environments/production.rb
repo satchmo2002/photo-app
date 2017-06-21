7b9b37fbfae297c6447d7489ab54dd528a21528d
satchmo2002 <satchwill.aaron@gmail.com>
2017-06-21 22:54:39 +0000

    6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  ##config.action_controller.asset_host = 'https://s3-us-east-2.amazonaws.com'
+  config.action_controller.asset_host = 'https://s3-us-east-2.amazonaws.com/photoappimagesbucket'
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
