71d0b887e33a31db1105ebfabe5256891b1d0c7e
satchmo2002 <satchwill.aaron@gmail.com>
2017-06-22 01:25:55 +0000

    8

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,17 @@
+ActionController::Base.asset_host = Proc.new { |source|
+  if source.ends_with?('.css')
+    "https://photo-app-asatchwill.herokuapp.com"
+  else
+    "https://s3-us-east-2.amazonaws.com/photoappimagesbucket"
+  end
+}
+
+#image_tag("rails.png")
+# => <img alt="Rails" src="http://assets.example.com/assets/rails.png" />
+#stylesheet_link_tag("application")
+# => <link href="http://myapp.herokuapp.com/assets/application.css" media="screen" rel="stylesheet" />
+
+
 if Rails.env.production?
     CarrierWave.configure do |config|
         config.fog_credentials = {
