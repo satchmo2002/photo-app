99e8fd2efe2fe66277f769eab75e4877ef187311
satchmo2002 <satchwill.aaron@gmail.com>
2017-06-22 16:45:59 +0000

    9

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,10 @@
-ActionController::Base.asset_host = Proc.new { |source|
-  if source.ends_with?('.css')
-    "https://photo-app-asatchwill.herokuapp.com"
-  else
-    "https://s3-us-east-2.amazonaws.com/photoappimagesbucket"
-  end
-}
+#ActionController::Base.asset_host = Proc.new { |source|
+#  if source.ends_with?('.css')
+#    "https://photo-app-asatchwill.herokuapp.com"
+#  else
+#    "https://s3-us-east-2.amazonaws.com/photoappimagesbucket"
+#  end
+#}
 
 #image_tag("rails.png")
 # => <img alt="Rails" src="http://assets.example.com/assets/rails.png" />
@@ -16,8 +16,8 @@ if Rails.env.production?
     CarrierWave.configure do |config|
         config.fog_credentials = {
             :provider => 'AWS',
-            :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-            :aws_secret_access_key => ENV['S3_SECRET_KEY']
+            :aws_access_key_id => ENV['S3_KEY'],
+            :aws_secret_access_key => ENV['S3_SECRET']
         }
     config.fog_directory = ENV['S3_BUCKET']
     end
